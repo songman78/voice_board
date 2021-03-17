7a23cc989dafe332aac1403ddf871a0f91d87d6b
songman78 <song.matsui@gmail.com>
2021-03-17 11:05:43 +0900

    tweetコントローラーの設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,8 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet,only: [:edit, :show]
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -11,9 +13,29 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+  end
+
   private
   def tweet_params
-    params.require(:tweet).permit(:name,:file, :text)
+    params.require(:tweet).permit(:name,:file, :text).merge(user_id:current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
  
 end
