93af084da6bf0f90216d46fbc7deae25b31eb301
songman78 <song.matsui@gmail.com>
2021-03-18 09:33:13 +0900

    バリデーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,10 @@ class Tweet < ApplicationRecord
   validates :text, presence: true  
   mount_uploader :file, AudiofileUploader
 
+  with_options presence: true do
+  validates :text
+  validates :file
+  end
   belongs_to :user
   has_many :comments
 end
