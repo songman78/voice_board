9f96fa0623b34253a08b2d5170a077856a590ba6
songman78 <song.matsui@gmail.com>
2021-03-18 23:18:02 +0900

    音声録音機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+console.log('Hello from application.js')
+
 //= require audiojs
 
 // Uncomment to copy all static images under ../images to the output folder and reference
