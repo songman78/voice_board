3c3e6554b9bf60c5224c715b40cf8e989febfee1
songman78 <song.matsui@gmail.com>
2021-03-22 00:23:07 +0900

    Action Cableを用いたコメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
+  def new
+    @comments = Comment.all
+    @comment = Comment.new
+  end
+
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}" 
+    @comment = Comment.new(comment_params)
+    redirect_to "/tweets/#{@comment.tweet.id}" 
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment
+    end
   end
 
   private
